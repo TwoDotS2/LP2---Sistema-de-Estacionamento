252ca0650aa4ad45deae118a43232081fe280b83
Arthur Silva <arthurti635@gmail.com>
2022-11-11 11:31:41 -0300

    # WARNING: head commit changed in the meantime
    Testando Git Eclipse

--- src/imd/ufrn/edu/view/Example.java
+++ src/imd/ufrn/edu/view/Example.java
@@ -3,5 +3,6 @@ package imd.ufrn.edu.view;
 public class Example {
     public static void main(String[] args) {
     	System.out.println("Teste Git Eclipse");
+    	System.out.println("Arthur Silva");
     }
 }
