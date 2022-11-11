fac9e39a8e4ffd5440c8344d9315813380fc029e
Arthur Silva <arthurti635@gmail.com>
2022-11-11 12:00:20 -0300

    Teste Git Eclipse

--- src/imd/ufrn/edu/view/Example.java
+++ src/imd/ufrn/edu/view/Example.java
@@ -4,5 +4,6 @@ public class Example {
     public static void main(String[] args) {
     	System.out.println("Teste Git Eclipse");
     	System.out.println("Arthur Silva");
+    	System.out.println("Teste 2");
     }
 }
