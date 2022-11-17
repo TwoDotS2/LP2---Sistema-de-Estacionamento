3a5d1358261e3d6901e783bb667322db9228176c
afonsoMoreira <AfonsoMoreira>
2022-11-17 08:22:50 -0300

    Funções geradoras de estruturas

--- src/imd/ufrn/edu/model/Patio.java
+++ src/imd/ufrn/edu/model/Patio.java
@@ -1,9 +1,26 @@
 package imd.ufrn.edu.model;
 
-import java.util.List;
+import java.util.Hashtable;
+
 
 public class Patio {
-    private List<Vaga> vagas;
+    private Hashtable<Integer, Vaga> vagas;
 
+    public Patio(){}
+
+    public void gerarPatio(int numeroTotalDeVagas){
+        vagas = new Hashtable<Integer, Vaga>(numeroTotalDeVagas);
+
+        for (int i = 1; i < numeroTotalDeVagas; i++) {
+            vagas.put(i, gerarVaga(i));
+        }
+    }
+
+    private Vaga gerarVaga(int numero){
+        Vaga vaga = new Vaga();
+        vaga.setNumero(numero);
+
+        return vaga;
+    }
 
 }
--- src/imd/ufrn/edu/model/Torre.java
+++ src/imd/ufrn/edu/model/Torre.java
@@ -1,12 +1,40 @@
 package imd.ufrn.edu.model;
 
-import java.util.List;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class Torre {
     private String nome;
-    private List<Apartamento> listaDeApartamentos;
+    private Map<Integer, Hashtable<Integer, Apartamento>> torre;
     private int andares;
+    public Torre() {}
 
+    public void gerarTorre(int andares, int apartamentosPorAndar){
+        torre = new Hashtable<Integer, Hashtable<Integer, Apartamento>>(andares);
+
+        for (Integer i = 1; i <= andares; i++){
+            torre.put(i,  gerarAndar( andares, apartamentosPorAndar ) );
+        }
+    }
+    private  Hashtable<Integer, Apartamento> gerarAndar(int andarAtual, int apartamentosPorAndar){
+        Hashtable<Integer, Apartamento> andar = new Hashtable<Integer, Apartamento>();
+
+        for(Integer i = 1; i <= apartamentosPorAndar; i++)
+            andar.put(
+                    i, gerarApartamento(andarAtual, i, apartamentosPorAndar)
+            );
+
+        return andar;
+    }
+    private Apartamento gerarApartamento(int andarAtual, Integer index, int apartamentosPorAndar){
+        Apartamento apt = new Apartamento();
+            apt.setAndar(andarAtual);
+            apt.setNumero(index);
+
+        return apt;
+    }
+
+    //Getters e Setters
     public String getNome() {
         return nome;
     }
@@ -19,7 +47,11 @@ public class Torre {
         return andares;
     }
 
-    public void setAndares(int andares) {
+    public void setAndares(short andares) {
         this.andares = andares;
+    }
+
+    public Map<Integer, Hashtable<Integer, Apartamento>> getTorre() {
+        return torre;
     }
 }
